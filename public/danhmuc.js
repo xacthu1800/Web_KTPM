081836619ed13a670208d81b7dbb1d5012a28ace
bin <nguyenvanbin.9a10@gmail.com>
2024-03-22 12:16:26 +0700

    add danhmuc

---
+++ public/danhmuc.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function changeImage(imageName) {
+  // Get all the images
+  var images = document.querySelectorAll('.left-column1 img');
+
+  // Remove the active class from all images
+  images.forEach(function(image) {
+    image.classList.remove('active');
+  });
+
+  // Add the active class to the image with the corresponding data-image attribute
+}
