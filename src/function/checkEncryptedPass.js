856a17544e7d446cc3055acb798f51cc1aa776a2
Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-05-20 21:03:09 +0700

    fix checkencrypted function

--- src/function/checkEncryptedPass.js
+++ src/function/checkEncryptedPass.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
-
-    
+const bcrypt = require('bcrypt');
 const {dataUser} = require('../config');
 
-async function checkEncryptedPass(account, password){
-    const acc = await dataUser.findOne({name:account})
-    if(!acc){
-        //console.log(201);
-        return 201
-   }
-   const isPasswordMatch = await bcrypt.compare(userRealPass, acc.password)
-    if(isPasswordMatch){
-        return 200
+async function checkEncryptedPass(account, password) {
+    const acc = await dataUser.findOne({name: account});
+    if (!acc) {
+        return 201;
+    }
+    const isPasswordMatch = await bcrypt.compare(password, acc.password);
+    if (isPasswordMatch) {
+        return 200;
     }
 
-    return 201
+    return 201;
 }
+
+/* // Hàm gọi checkEncryptedPass và log kết quả
+async function run() {
+    const result = await checkEncryptedPass('123', '123');
+    console.log(result);
+}
+
+run(); */
 
 module.exports = checkEncryptedPass
