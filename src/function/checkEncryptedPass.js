1f7e8b4cd810df834ad24dd3d9a4f3f5992f4154
Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-05-20 20:55:07 +0700

    fix checkenCryptedpass function

--- src/function/checkEncryptedPass.js
+++ src/function/checkEncryptedPass.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
-// Connect to MongoDB database
-mongoose.connect("mongodb+srv://nguyenvanbin9a10:I5m3xytYKQjOH4D1@cluster1.xxrs0yh.mongodb.net/Login")
-    .then(() => {
-        console.log('Connected to database');
-    })
-    .catch((err) => {
-        console.error('Database connection error:', err);
-    });
     
 const {dataUser} = require('../config');
 
@@ -17,12 +10,12 @@ async function checkEncryptedPass(account, password){
         //console.log(201);
         return 201
    }
-   const userRealPass = acc.password
-    if(userRealPass == password){
-        return 201
+   const isPasswordMatch = await bcrypt.compare(userRealPass, acc.password)
+    if(isPasswordMatch){
+        return 200
     }
 
-    return 200
+    return 201
 }
 
 module.exports = checkEncryptedPass
